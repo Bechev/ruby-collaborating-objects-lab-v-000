c41c13cb33c154dedc07bb649778591ff1c146ad
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-13 11:19:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,32 +7,33 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-
-  end
-
-  def add_song(new_song)
-    @songs << new_song
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
     @@all
   end
 
-  def songs
-    @songs
+  def add_song(new_song)
+    @songs << new_song
   end
 
   def self.find_or_create_by_name(artist_name)
-    artist_match = @@all.detect { |artist| artist_name == artist.name
+    artist_match = self.all.detect { |artist| artist_name == artist.name
     }
     if not artist_match
       artist_match = Artist.new(artist_name)
     end
   end
+
+  def save
+    @@all << self
+  end
+
+
+  def songs
+    @songs
+  end
+
 
   def print_songs
     @songs.each do |song|
